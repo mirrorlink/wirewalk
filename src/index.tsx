05b24c14daafcea102e05db530da55b187cb21bf
Danilo Ganzella <ganzelladanilo@gmail.com>
2021-04-17 19:07:17 -0300

    favicon

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Helmet } from "react-helmet";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Helmet>
+        <title>Wirewalk</title>
+        <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+    </Helmet>
   </React.StrictMode>,
   document.getElementById("root")
 );
