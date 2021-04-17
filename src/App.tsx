8dac79a9dd466856432c855b90c813f0d3e083d0
Danilo Ganzella <ganzelladanilo@gmail.com>
2021-04-17 18:55:14 -0300

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import anim from "./images/construction.gif";
+
+
+function App(): any {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className="MainText">
+          An Action-RPG Where you Raid virus-infected digital systems for the lols.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <div className="bottomRight">
+          <p className="subText">
+            Under construction..
+          </p>
+          <img src={anim} className="construction" />
+
+        </div>
+
       </header>
     </div>
   );
 }
+
+/*
+//import MailchimpSubscribe from "react-mailchimp-subscribe";
+const url: string = "//xxxx.us13.list-manage.com/subscribe/post?u=zefzefzef&id=fnfgn";
+        <div className="MailChimp">
+          <p className="subscribe">
+            Subscribe for news!
+          </p>
+          <MailchimpSubscribe url={url}/>
+        </div>
+*/
 
 export default App;
